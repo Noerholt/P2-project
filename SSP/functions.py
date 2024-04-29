f1c7461662a09b7039fb51e9d14900d7788bd735
SimonSeaAxe <148970792+SimonSeaAxe@users.noreply.github.com>
2024-04-29 13:35:57 +0200

    Update functions.py

--- SSP/functions.py
+++ SSP/functions.py
@@ -8,16 +8,16 @@ humidity=80 #relativ luftfugtighed fra 0-100%
 deHumOn=False #status for if dehumidifier is turned on
 filterRate=20 #the rate which the filter lets water through
 
-def deHumControl(humidity, StationWaterTank.GetFluidAmount()):
+def deHumControl(humidity):
     if humidity<30 or StationWaterTank.FluidLevel>=80:
         deHumOn=False
     else:
         deHumOn=True
 
-def waterTankControl(GetFluidAmount(StationWaterTank)):
+def waterTankControl():
+    
     if StationWaterTank.FluidLevel>=95:
         wInputClose()
-        
         
         
         
