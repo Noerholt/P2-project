14116a7e1442a10e791706b608848f5f68a7bdd5
MesterMaggen <101719834+MesterMaggen@users.noreply.github.com>
2024-04-29 13:46:46 +0200

    Update functions.py

--- SSP/functions.py
+++ SSP/functions.py
@@ -1,3 +1,9 @@
+StationSoapTank = SoapTank()
+StationWaterTank = Water_Tank()
+CleanerMopTank = Tank()
+WasteMopTank = Tank()
+
+
 humidity=80 #relativ luftfugtighed fra 0-100%
 deHumOn=False #status for if dehumidifier is turned on
 filterRate=20 #the rate which the filter lets water through
