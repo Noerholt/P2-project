8e5c3cdb023df281fc1cf481adfd86bacbf6b95f
MesterMaggen <101719834+MesterMaggen@users.noreply.github.com>
2024-04-29 13:47:35 +0200

    Update functions.py

--- SSP/functions.py
+++ SSP/functions.py
@@ -1,7 +1,7 @@
-StationSoapTank = SoapTank()
-StationWaterTank = Water_Tank()
-CleanerMopTank = Tank()
-WasteMopTank = Tank()
+stationSoapTank = SoapTank()
+stationWaterTank = Water_Tank()
+sleanerMopTank = Tank()
+sasteMopTank = Tank()
 
 
 humidity=80 #relativ luftfugtighed fra 0-100%
@@ -9,14 +9,14 @@ deHumOn=False #status for if dehumidifier is turned on
 filterRate=20 #the rate which the filter lets water through
 
 def deHumControl(humidity):
-    if humidity<30 or StationWaterTank.FluidLevel>=80:
+    if humidity<30 or stationWaterTank.FluidLevel>=80:
         deHumOn=False
     else:
         deHumOn=True
 
 def waterTankControl():
     
-    if StationWaterTank.FluidLevel>=95:
+    if stationWaterTank.FluidLevel>=95:
         wInputClose()
         
         
