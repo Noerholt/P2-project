53f69ccae21e80bc809fbcac51083cbbf4d9eb17
Victor Lucassen <vict7255@gmail.com>
2024-05-03 13:34:11 +0200

    uhuh

--- Project/Simulation.py
+++ Project/Simulation.py
@@ -11,19 +11,26 @@ RL = Robolink()
 robot = RL.Item('MyCoBot_320')
  
 # get the home target and the welding targets:
-home = RL.Item('Target 1')
-target = RL.Item('Target 2')
+home = RL.Item('home')
+pillA = RL.Item('pill A')
+pillAapp = RL.Item('pill A app')
+pillB = RL.Item('pill B')
+pillBapp = RL.Item('pill B app')
 # get the pose of the target (4x4 matrix):
-poseref = target.Pose()
 
-program.runProgram([["B","A"],["A","A","B"]])
+#program.runProgram([["B","A"],["A","A","B"]])
 
 # move the robot to home, then to the center:
 robot.MoveJ(home)
-robot.MoveJ(target)
+robot.MoveJ(pillAapp)
+robot.MoveL([-48.946770, -65.509573, -96.859159, 72.368732, 90.000000, 41.053230])
+
+robot.MoveL(pillAapp)
 x = 1
 
 if x == 1:
     robot.MoveJ(home)
-    robot.MoveJ(target)
+    robot.MoveJ(pillBapp)
+    robot.MoveL(pillB)
+    robot.MoveL(pillAapp)
     print("Final destination!")
