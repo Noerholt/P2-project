15b7fca42d3503d18d05c43e7ba7220b8a223b67
Victor Lucassen <vict7255@gmail.com>
2024-05-08 12:59:11 +0200

    Update Simulation.py

--- Project/Simulation.py
+++ Project/Simulation.py
@@ -1,5 +1,7 @@
 from robolink import *    # API to communicate with RoboDK
 from robodk import *      # basic matrix operations
+import inverseKinematics as IK
+import numpy as np
 
 import program
  
@@ -13,7 +15,7 @@ robot = RL.Item('MyCoBot_320')
 
 tool = RL.Item('End Effector')
 
-RL.ShowRoboDK()
+#RL.ShowRoboDK()
 
 home = RL.Item('home')
 pillA = RL.Item('A pill')
@@ -37,6 +39,59 @@ dagPeriode = ["morgen", "middag", "aften", "nat"]
 FullList =[["A","A","B","A"],["B","A","B"],["A","B"],["B","A"]]
 
 #sync.send_coords(home)
+
+def move_perfect_line(targetJoints):
+
+    #coordRobot = mc.get_angles()
+
+    for x in range(10):
+
+        joints = robot.Joints().list()
+
+        difference = [a-b for b,a in zip(joints,targetJoints)]
+
+        moveSize = [x/10 for x in difference]
+
+        print(moveSize)
+
+        print(difference)
+
+        robot.MoveJ([a+b for a,b in zip(joints,moveSize)])
+
+    #mc.send_angles((difference/20))
+
+def move_perfect_line2(startEuler, endEuler):
+
+    startPose = IK.TransformDesired(startEuler[0],startEuler[1],startEuler[2],startEuler[3],startEuler[4],startEuler[5])
+    endPose = IK.TransformDesired(endEuler[0],endEuler[1],endEuler[2],endEuler[3],endEuler[4],endEuler[5])
+
+    num_steps = 100
+
+    viapointJoints2 = [0,0,0,0,0,0]
+
+    for i in range(num_steps + 1):
+        viapoint_pose = startPose + (endPose - startPose) * i / num_steps
+        
+        viapointJoints = IK.CalculateThetaValues(viapoint_pose)
+
+        #print(viapointJoints)
+
+        for i in range(6):
+
+            viapointJoints2[i] = viapointJoints[0,i]*180/math.pi
+
+        #viapointJoints2 = [viapointJoints[0,0],viapointJoints[0,1],viapointJoints[0,2],viapointJoints[0,3],viapointJoints[0,4],viapointJoints[0,5]]
+        print(viapointJoints2)
+
+        robot.MoveJ(viapointJoints2)
+
+move_perfect_line2([70,-250,80,180,0,180], [-220, -150,80,180,0,180])
+
+#robot.MoveJ([0,0,0,0,0,0])
+
+
+#print(robot.Pose())
+#move_perfect_line([-126.236618, -36.785256, -104.413971, 51.199227, 90.000000, -36.236618])
 
 def runProgram(patientList):
 
@@ -122,5 +177,5 @@ def runProgram(patientList):
 
             #sync.send_coords(approachPillContainer[t], 15, 1)
 
-runProgram(FullList)
+#runProgram(FullList)
 
