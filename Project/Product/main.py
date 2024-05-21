62bfa0a83730ef76d91e4405f155be867b9aeb9a
Jens Jørgensen <147208366+J-PJ@users.noreply.github.com>
2024-05-21 20:58:48 +0200

    good

--- Project/Product/main.py
+++ Project/Product/main.py
@@ -6,7 +6,7 @@ from pymycobot.mycobot import MyCobot
 from pymycobot import PI_PORT, PI_BAUD
 
 os.system('cls')
-mc = MyCobot("COM1", 115200)
+mc = MyCobot("COM3", 115200)
 mc.power_on()
 mc.sync_send_angles(home.coords,30)
 time.sleep(1)
@@ -18,6 +18,7 @@ List2 =[["A","A","A","A","A","A","A","A","A"],["B","B","B","B","B","B","B","B","
 List3 =[["A","A","A","A","A"],["B","B","B","B","B"],["A","A","A","A"],["B","B","B","B"]]
 List4 =[["A","B"],["A","B"],["A","B"],["A","B"]]
 List5 =[["A"],["A"],["A"],["A"]]
+List6 =[["A","A","A","B","B",],["A","A","B","B","B"],["B","B","A","A"],["B","B","A","A"]]
 
 targetPositions = {'A': ApproachPillA.coords, 'B': ApproachPillB.coords}
 dispenserPositions = [MorningApproach.coords, AfternoonApproach.coords, EveningApproach.coords, NightApproach.coords]
